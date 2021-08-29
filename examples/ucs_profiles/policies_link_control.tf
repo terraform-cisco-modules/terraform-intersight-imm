170a260117bcef1c39e29d55afd006737cffe7de
scotttyso <tyscott@cisco.com>
2021-08-28 22:24:06 -0400

    condensing examples to match easyucs

---
+++ examples/ucs_profiles/policies_link_control.tf
@@ -0,0 +1,39 @@
+#____________________________________________________________
+#
+# Example Intersight Link Control Policies Module
+# GUI Location: Policies > Create Policy > Link Control
+#____________________________________________________________
+
+module "link_control_example" {
+  depends_on = [
+    data.intersight_organization_organization.org_moid
+  ]
+  source      = "terraform-cisco-modules/imm/intersight//modules/policies_link_control"
+  description = "Link Control Policy Example."
+  name        = "example"
+  org_moid    = local.org_moid
+  tags        = var.tags
+}
+
+
+#______________________________________________
+#
+# Example with Default Values
+#______________________________________________
+
+/*
+
+module "link_control_defaults" {
+  depends_on        = [
+    data.intersight_organization_organization.org_moid
+  ]
+  source            = "terraform-cisco-modules/imm/intersight//modules/policies_link_control"
+  description       = ""
+  udld_admin_state  = "Enabled"
+  udld_mode         = "normal"
+  name              = "link_control"
+  org_moid          = local.org_moid
+  tags              = var.tags
+}
+
+*/
